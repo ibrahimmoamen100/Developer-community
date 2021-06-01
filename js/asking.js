aa6366d52a122658fd57c6674a77362d50b42628
ibrahimmoamen100 <ibrahim.moamen100@gmail.com>
2021-06-01 19:05:56 +0200

    update who we are

--- js/asking.js
+++ js/asking.js
@@ -2,17 +2,26 @@ let nationSelect = document.querySelector(".nationality__select");
 let OptionNationSelect = document.querySelectorAll(
   ".nationality__select option"
 );
+
+let inputNat = document.querySelector(".dropdown-trigger");
+
+console.log(inputNat);
+
+inputNat.addEventListener("change", (e) => {
+  console.log(e.target.value);
+});
+
 // console.log(OptionNationSelect);
 
 // for (opt of OptionNationSelect) {
 //   console.log(opt);
 // }
 
-function getSelectValue() {
-  let realSelect = document.querySelectorAll(".dropdown-content > li");
-  for (ele of realSelect) {
-    let selectedEele = ele.classList.contain("selected");
-    console.log(selectedEele);
-  }
-}
-getSelectValue();
+// function getSelectValue() {
+//   let realSelect = document.querySelectorAll(".dropdown-content > li");
+//   for (ele of realSelect) {
+//     let selectedEele = ele.classList.contain("selected");
+//     console.log(selectedEele);
+//   }
+// }
+// getSelectValue();
